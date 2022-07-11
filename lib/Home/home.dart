12c8e53ba620fb7a191f37d40bc814fd9c2f6659
JSGONZALEZ26 <jsgonzalez13@utpl.edu.ec>
2022-07-11 16:38:48 -0500

    cambios a un widget

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -10,7 +10,7 @@ class HomeStart extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text("Center"),
+        child: Text("Mercury Booking"),
       )
     );
   }
