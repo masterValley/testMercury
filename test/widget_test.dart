1ee29001431a89d1a7b8994c2087360af2d8b77a
JSGONZALEZ26 <jsgonzalez13@utpl.edu.ec>
2022-07-12 07:51:37 -0500

    Test de los tests 5

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mercury_app/Home/home.dart';
 
 import 'package:mercury_app/main.dart';
 
@@ -14,11 +15,7 @@ void main() {
   testWidgets('finds a Text widget', (tester) async {
   // Build an App with a Text widget that displays the letter 'H'.
   await tester.pumpWidget(const MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child:Text("Mercury\nBooking.")
-      ),
-    ),
+    home: HomeStart()
   ));
 
   // Find a widget that displays the letter 'H'.
